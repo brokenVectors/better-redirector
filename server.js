4ad0e67ecb7dc5373da354a4ecd0012c64132a8f
brokenVectors <brokenvectors@gmail.com>
2022-01-22 23:47:37 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+    console.log("Server listening on port " + port);
+})
